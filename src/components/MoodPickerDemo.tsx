9222b4d1d2241d205cd61a020b68993e5434fdd6
Bukola Omojowo <bomojowo@gmail.com>
2021-06-28 12:13:54 +0100

    Removed console logs and added 3 more emoji buttons

--- src/components/MoodPickerDemo.tsx
+++ src/components/MoodPickerDemo.tsx
@@ -5,42 +5,31 @@ function MoodPickerDemo(): JSX.Element {
     useState("confused");
 
   const handleMoodChangeToHappy = () => {
-    const nextMood = "happy";
-    queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "😀 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
+   
+    queueRerenderWithNewMoodValue("happy");
   };
 
   const handleMoodChangeToConfused = () => {
-    const nextMood = "confused";
-    queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "🤔 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
+    
+    queueRerenderWithNewMoodValue("confused");
   };
 
   const handleMoodChangeToSad = () => {
-    const nextMood = "sad";
-    queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "😢 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
+    
+    queueRerenderWithNewMoodValue("sad");
   };
 
-  console.log(
-    "Component is rendering with a mood value of",
-    moodValueFromCurrentRender
-  );
+  const handleMoodChangeToAngry = () => {
+    queueRerenderWithNewMoodValue("angry")
+  };
+
+  const handleMoodChangeToSick = () => {
+    queueRerenderWithNewMoodValue("sick")
+  };
+
+  const handleMoodChangeToLoveStruch = () => {
+    queueRerenderWithNewMoodValue("love struck")
+  };
 
   return (
     <>
@@ -49,6 +38,10 @@ function MoodPickerDemo(): JSX.Element {
       <button onClick={handleMoodChangeToHappy}>😀</button>
       <button onClick={handleMoodChangeToConfused}>🤔</button>
       <button onClick={handleMoodChangeToSad}>😢</button>
+      <button onClick={handleMoodChangeToAngry}>😡</button>
+      <button onClick={handleMoodChangeToSick}>🤮</button>
+      <button onClick={handleMoodChangeToLoveStruch}>😍</button>
+
     </>
   );
 }
